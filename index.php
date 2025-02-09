a6d56dcd31dbe5d33b67c3c2918993228be348c6
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-02-09 12:44:06 -0300

    Criação função de limpar a tela

--- index.php
+++ index.php
@@ -1,49 +1,69 @@
 <?php
-    $usuarios = [
-        [
-            "usuario" => "admin",
-            "senha" => "12345"
-        ]
-    ];
+
+$usuarios = [
+    [
+        "usuario" => "admin",
+        "senha" => "12345"
+    ]
+];
+
+$produtos = [
+    [
+        "id" => 1,
+        "nome" => "café",
+        "preco" => 23.99,
+        "estoque" => 10
+    ]
+];
 
 
-    function logar($usuario, $senha){
-        global $usuarios;
-        foreach($usuarios as $user){
-            if($user["usuario"] == $usuario && $user["senha"] == $senha){
-                return true;
-            }
+function logar($usuario, $senha)
+{
+    global $usuarios;
+    foreach ($usuarios as $user) {
+        if ($user["usuario"] == $usuario && $user["senha"] == $senha) {
+            return true;
         }
-
-        return false;
     }
 
-    echo chr(27).chr(codepoint: 91).'H'.chr(27).chr(91).'J'; // ^[H^[J
+    return false;
+}
 
+function limparTela()
+{
+    echo "\033[H\033[J";
 
-    while(true){
+}
 
-        echo "[1] Login\n[2] Sair\n";
-        $escolha = readline("-");
-        if($escolha == 1){
-            echo chr(27).chr(codepoint: 91).'H'.chr(27).chr(91).'J'; // ^[H^[J
-            $usuario = readline("Usuário: ");
-            $senha = readline("Senha: ");
-            if(logar($usuario, $senha)){
-                while(true){
-                    //Menu do usuario logado
-                }
-            } else{
-                echo chr(27).chr(codepoint: 91).'H'.chr(27).chr(91).'J'; // ^[H^[J
-                echo "Senha ou usuarios incorretos!\n";
+limparTela();
+
+while (true) {
+    echo "[1] Login\n[2] Sair\n";
+    $escolha = readline("-");
+
+    if ($escolha == 1) {
+        limparTela();
+        $usuario = readline("Usuário: ");
+        $senha = readline("Senha: ");
+
+        if (logar($usuario, $senha)) {
+            while (true) {
+                //Menu do usuario logado'
+                limparTela();
+                echo "[1]Realizar venda\n[2]Verificar logs\n[3]Cadastrar novo usuário\n[4]Deslogar\n";
+                $escolha = readline("-");
+
             }
-
-        } else if( $escolha == 2){
-            break;
+        } else {
+            limparTela();
+            echo "Senha ou usuarios incorretos!\n";
         }
-
-
+    } else if ($escolha == 2) {
+        break;
     }
+
+
+}
 
 
 
